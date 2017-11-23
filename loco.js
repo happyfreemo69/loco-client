a0df013e02588dd289709b7abdbbad152e6919f3
mpech <maxime.pech@freemo.blue>
2017-11-23 17:53:27 +0100

    [TG-4194] create asset and translation

--- loco.js
+++ loco.js
@@ -1,6 +1,6 @@
 #!/usr/local/bin/node
 var https = require('https');
-
+var querystring = require('querystring');
 var exports = module.exports;
 function _get(o,url){
     var P = exports.Promise || Promise;
@@ -38,8 +38,67 @@ function _get(o,url){
     });
 }
 
+function _post(o,url, body){
+    var P = exports.Promise || Promise;
+    return new P(function(resolve, reject){
+        var path = url + (url.includes('?')?'&':'?')+'key='+o.loco_apiKey;
+        var post_data = querystring.stringify(body);
+        var req = https.request({
+            method:'POST',
+            path:path,
+            hostname:'localise.biz',
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(post_data)
+            }
+        }, function(res){
+            var s = '';
+            res.on('data', function(chunk){
+                if(res.statusCode != 200 && res.statusCode !=201){
+                    return reject('failed to get'+res.statusCode+' '+chunk.toString());
+                }
+                return s+=chunk.toString();
+            });
+            res.on('end', function(){
+                try{
+                    var d = JSON.parse(s);
+                    return resolve(d)
+                }catch(e){
+                    return reject(e);
+                }
+            })
+        });
+        req.on('error', function(err){
+            return reject('failed to get'+chunk.toString());
+        });
+        req.write(post_data);
+        req.end();
+    });
+}
+
+
 exports.getTranslation = function(o, id, locale){
     return _get(o, '/api/translations/'+id+'/'+locale);
+}
+
+/**
+ * [createTranslation description]
+ * @param  {[type]} o    [description]
+ * @param  {{id, name}} body [description]
+ * @return {[type]}      [description]
+ */
+exports.createTranslation = function(o, body, locale){
+    return _post(o, '/api/translations/'+body.id+'/'+locale, {name:body.name});
+}
+
+/**
+ * [createAsset description]
+ * @param  {[type]} o    [description]
+ * @param  {{id, name}} body name for the default source language
+ * @return {[type]}      [description]
+ */
+exports.createAsset = function(o, body){
+    return _post(o, '/api/assets', body);
 }
 
 exports.getLocales = function(o){
@@ -54,8 +113,14 @@ if(!module.parent){
     var optimist = require('optimist')
         .usage(
 `translating: 
-    
+translate:
     node loco.js -t idToTranslate -l fr
+
+create asset:
+    node loco.js -c -b id
+
+create translation for asset
+    node loco.js -c -t idToTranslate -l fr -b data
 
 exporting:
     
@@ -67,6 +132,10 @@ listing all locales:
         .options('t', {
             alias : 'translate',
             describe: 'translate the id'
+        })
+        .options('c', {
+            alias : 'create',
+            describe: 'create a translation'
         })
         .options('l', {
             alias : 'locale',
@@ -80,6 +149,10 @@ listing all locales:
             alias : 'list-locales',
             describe: 'list all existing locales'
         })
+        .options('b', {
+            alias : 'body',
+            describe: 'data for the translation'
+        })
     var argv = optimist.argv;
     if(argv.help){
         optimist.showHelp()
@@ -89,10 +162,29 @@ listing all locales:
         throw 'missing export LOCO_APIKEY=something';
     }
     const cred = {loco_apiKey: process.env.LOCO_APIKEY};
-    if(!argv.t && !argv.e && !argv.a){
+    if(!argv.t && !argv.e && !argv.a && !argv.c){
         optimist.showHelp()
         process.exit(0);   
     }
+
+    if(argv.c){
+        if(argv.t){
+            if(!argv.locale){
+                throw 'expect locale';
+            }
+            if(!argv.body){
+                throw 'expect body';
+            }
+            return exports.createTranslation(cred, {id:argv.t, name:argv.body}, argv.locale).then(x=>{
+                console.log(JSON.stringify(x,null,1));
+            })
+        }
+
+        return exports.createAsset(cred, {id:argv.body, name:argv.body}).then(x=>{
+            console.log(JSON.stringify(x,null,1));
+        })
+    }
+
     if(argv.t){
         if(!argv.l){
             throw 'expect locale';
